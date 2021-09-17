f225ccf1ad7a452cd7e9cac76e42f52205dba8ef
rudxde <kj4derechte@gmail.com>
2021-09-17 22:42:59 +0200

    feat(*): add more keys/commands

--- packages/desktop-client/src/send-key.ts
+++ packages/desktop-client/src/send-key.ts
@@ -21,6 +21,17 @@ export class SendKey {
         const mode = 1;
         this.stdIn?.write(Buffer.from([0, mode, code]));
     }
+
+    moveWindow(directionCode: number): void {
+        const modePress = 1;
+        const modeDown = 2;
+        const modeUp = 3;
+        this.stdIn?.write(Buffer.from([
+            0, modeDown, SendKeyCodes.VK_LWIN,
+            0, modePress, directionCode,
+            0, modeUp, SendKeyCodes.VK_LWIN
+        ]));
+    }
 }
 
 // https://docs.microsoft.com/de-de/windows/win32/inputdev/virtual-key-codes
@@ -31,5 +42,41 @@ export enum SendKeyCodes {
     VK_MEDIA_NEXT_TRACK = 0xB0,
     VK_MEDIA_PREV_TRACK = 0xB1,
     VK_MEDIA_STOP = 0xB2,
-    VK_MEDIA_PLAY_PAUSE = 0xB3
+    VK_MEDIA_PLAY_PAUSE = 0xB3,
+    VK_ESCAPE = 0x1B,
+    VK_LWIN = 0x5B,
+    VK_LEFT = 0x25,
+    VK_UP = 0x26,
+    VK_RIGHT = 0x27,
+    VK_DOWN = 0x28,
+    VK_SPACE = 0x20,
+    A = 0x41,
+    B = 0x42,
+    C = 0x43,
+    D = 0x44,
+    E = 0x45,
+    F = 0x46,
+    G = 0x47,
+    H = 0x48,
+    I = 0x49,
+    J = 0x4A,
+    K = 0x4B,
+    L = 0x4C,
+    M = 0x4D,
+    N = 0x4E,
+    O = 0x4F,
+    P = 0x50,
+    Q = 0x51,
+    R = 0x52,
+    S = 0x53,
+    T = 0x54,
+    U = 0x55,
+    V = 0x56,
+    W = 0x57,
+    X = 0x58,
+    Y = 0x59,
+    Z = 0x5A,
+    VK_F11 = 0x7A,
+    VK_OEM_COMMA = 0xBC,
+    VK_OEM_PERIOD = 0xBE,
 }
--- packages/server/src/filter-commands.ts
+++ packages/server/src/filter-commands.ts
@@ -9,6 +9,45 @@ export function filterCommands(command: string): string {
         case Commands.VOLUME_DOWN:
         case Commands.VOLUME_MUTE:
         case Commands.VOLUME_UP:
+        case Commands.VK_ESCAPE:
+        case Commands.VK_LEFT:
+        case Commands.VK_UP:
+        case Commands.VK_RIGHT:
+        case Commands.VK_DOWN:
+        case Commands.VK_SPACE:
+        case Commands.WINDOW_LEFT:
+        case Commands.WINDOW_UP:
+        case Commands.WINDOW_RIGHT:
+        case Commands.WINDOW_DOWN:
+        case Commands.A:
+        case Commands.B:
+        case Commands.C:
+        case Commands.D:
+        case Commands.E:
+        case Commands.F:
+        case Commands.G:
+        case Commands.H:
+        case Commands.I:
+        case Commands.J:
+        case Commands.K:
+        case Commands.L:
+        case Commands.M:
+        case Commands.N:
+        case Commands.O:
+        case Commands.P:
+        case Commands.Q:
+        case Commands.R:
+        case Commands.S:
+        case Commands.T:
+        case Commands.U:
+        case Commands.V:
+        case Commands.W:
+        case Commands.X:
+        case Commands.Y:
+        case Commands.Z:
+        case Commands.VK_F11:
+        case Commands.VK_OEM_COMMA:
+        case Commands.VK_OEM_PERIOD:
             return command;
         case Commands.PING:
         case "ping":
